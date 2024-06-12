970862e49a23c04014dab0727b084c937e05d087
antony <202100354e@utea.edu.pe>
2024-06-12 13:50:34 -0500

    enums

--- 01_clases_objetos.dart
+++ 01_clases_objetos.dart
@@ -1,25 +1,13 @@
 void main(){
-  Trabajador pepe = Trabajador();
-  pepe.obtenerNombre();
-  pepe.calcular();
-
-
-}
-abstract class Persona{
-  String? nombre;
-  void obtenerNombre();
+  print(Colores.values);
+  Colores.values.forEach((item){
+//    print(item.index);
+print(item.name);
+  });
 }
 
-mixin Administrador{
-  String? area;
-  void calcular(){
-    print("Calculando Salario");
-  }
-}
-
-class Trabajador extends Persona with Administrador{
-  @override
-  void obtenerNombre(){
-    print("El nombre del trabajador");
-  }
+enum Colores {
+  rojo,
+  azul,
+  amarillo
 }
