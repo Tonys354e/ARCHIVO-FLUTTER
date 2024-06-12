1128bcddb404d91a309183c2552340ab1b653404
antony <202100354e@utea.edu.pe>
2024-06-12 13:20:54 -0500

    instanciacion

--- 01_clases_objetos.dart
+++ 01_clases_objetos.dart
@@ -1,5 +1,22 @@
 void main (){
 
+Telefono miTelefono = Telefono("motorola","2223333444",true);
+print(miTelefono);
+print(miTelefono.marca);
+print(miTelefono.numero);
+print(miTelefono.cargaRapida);
+
+  miTelefono.marca = "LG";
+  print(miTelefono.marca);
+  miTelefono.llamar();
+
+  String numeroTelefono =miTelefono.obternerNumero();
+  print(numeroTelefono);
+
+
+Telefono lg = Telefono("LG","226348376",true);
+print(lg);
+print(lg.marca);
 
 }
   //atributos
@@ -15,7 +32,7 @@ class Telefono{
   // metodo
 
   void llamar (){
-    print("el telefono esta llamando");
+    print("el telefono esta llamando.....");
 
   }
 
