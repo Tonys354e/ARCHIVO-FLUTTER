26ff7ede35017d9e2d49ea4a5266348780d1ebb3
antony <202100354e@utea.edu.pe>
2024-06-12 13:51:18 -0500

    datatime

--- 01_clases_objetos.dart
+++ 01_clases_objetos.dart
@@ -1,13 +1,19 @@
 void main(){
-  print(Colores.values);
-  Colores.values.forEach((item){
-//    print(item.index);
-print(item.name);
-  });
-}
+  DateTime fechaActual = DateTime.now();
+  print(fechaActual.year);
+  print(fechaActual.month);
+  print(fechaActual.day);
+  print(fechaActual.hour);
+  print(fechaActual.minute);
+  String fecha = "2020-07-20 20:12:23";
+  
+  DateTime fechaPasada = DateTime.parse(fecha);
+  print(fechaPasada);
+  DateTime dt1 = DateTime.parse("2020-07-20 20:12:23");
+  DateTime dt2 = DateTime.parse("2021-05-11 11:12:23");
 
-enum Colores {
-  rojo,
-  azul,
-  amarillo
+  Duration diferencia= dt2.difference(dt1);
+  print(diferencia.inDays);
+  print(diferencia.inHours);
+  print(diferencia.inMinutes);
 }
